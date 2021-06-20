fdadbe3f56a6ee0d0fa992c312dc2909a19a59da
Carlos <carlos@xtrene.com>
2021-06-21 00:06:07 +0200

    Agregar archivos de proyecto.

---
+++ Tarea04_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tarea04_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce nombre: ");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Introduce ciudad:");
+            String ciudad = Console.ReadLine();
+
+            Console.WriteLine("Hola  {0}, bienvenido/a a {1}.",nombre,ciudad);
+
+            Console.ReadKey();
+        }
+    }
+}
